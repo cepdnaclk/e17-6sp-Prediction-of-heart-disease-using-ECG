567b60b6ec8670dd9e29e42df1386cb1c4e32812
ruchika <pereraruchika7@gmail.com>
2022-03-27 20:17:26 +0530

    Navigation Bar - In Progress

--- 6sp-web_app/src/App.js
+++ 6sp-web_app/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
-function App() {
+import HomePage from './pages/Home';
+import LoginPage from './pages/Login';
+import RegistrationPage from './pages/Registration';
+import NavBar from './components/NavBar';
+
+function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div>
+        <NavBar />
+        <Routes>
+          <Route path="/">
+            <Route index element={<HomePage />} />
+            <Route path="login" element={<LoginPage />} />
+            <Route path="registration" element={<RegistrationPage />} />
+          </Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
+
 }
 
 export default App;
