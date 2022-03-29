4dfc803fcc71d7250659a3cc3b01b96ea3f8fb8e
ruchika <pereraruchika7@gmail.com>
2022-03-29 16:37:08 +0530

    Navigation Bar1 completed

--- 6sp-web_app/src/components/NavBar1.js
+++ 6sp-web_app/src/components/NavBar1.js
@@ -2,35 +2,43 @@ import { Link } from 'react-router-dom';
 
 import classes from './NavBar.module.css';
 
-function NavBar1(){
+function NavBar1(props){
     return(
+        <><meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
         <header className={classes.header}>
             <nav>
                 <ul>
+                    <li></li>
                     <li>
                         <div className={classes.hometitle}>HEART DISEASE PREDICTOR</div>
                     </li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
                     <li>
-                        <Link to="">Features</Link>
+                        <Link to={props.link1} className={classes.small_links}>{props.link1_name}</Link>
                     </li>
                     <li>
-                        <Link to="">Pricing</Link>
+                        <Link to={props.link2} className={classes.small_links}>{props.link2_name}</Link>
                     </li>
                     <li>
-                        <Link to="">Contact</Link>
+                        <Link to={props.link3} className={classes.small_links}>{props.link3_name}</Link>
                     </li>
                     <li></li>
                     <li></li>
-                    <li className={classes.login}>
-                        <Link to="/login">Log In</Link>
+                    <li></li>
+                    <li></li>
+                    <li>
+                        <Link to={props.link4} className={classes.links}>{props.link4_name}</Link>
                     </li>
                     <li></li>
-                    <button>
-                        <Link to="/registration">Create Account</Link>
+                    <button className={classes.button}>
+                        <Link to={props.link5} className={classes.text}>{props.link5_name}</Link>
                     </button>
                 </ul>
             </nav>
-        </header>
+        </header></>
     );
 }
 
--- 6sp-web_app/src/pages/Login.js
+++ 6sp-web_app/src/pages/Login.js
@@ -1,5 +1,14 @@
+import NavBar1 from '../components/NavBar1';
+
 function LoginPage(){
-    return <div>Login Page</div>;
+    return (
+        <>
+        <NavBar1 link1="" link1_name="Features" link2="" link2_name="Pricing" link3="" link3_name="Contact" link4="/" link4_name="Back to Homepage" link5="/registration" link5_name="Create Account" />
+        <div>
+            Login Page
+        </div>
+        </>
+    );
 }
 
 export default LoginPage;
